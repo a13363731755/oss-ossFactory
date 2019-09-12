d4d7c6ab74a883273a4b44dbfb3fc88ad0d5fffb
任少鹏 <ren.shaopeng@genecast.com.cn>
2019-09-12 15:44:29 +0800

    AliyunOss补丁

--- src/Facades/AliyunOss.php
+++ src/Facades/AliyunOss.php
@@ -1,6 +1,7 @@
 <?php
 namespace AliOss\OssFactory\Src\Facades;
 
+use AliOss\OssFactory\src\OssServiceInterface;
 use Illuminate\Support\Facades\Facade;
 
 /**
@@ -10,6 +11,22 @@ use Illuminate\Support\Facades\Facade;
  * @category   Laravel Oss
  * @version    1.2.2
  * @package    oss/ossFactory
+ *
+ * @method static array listBuckets()
+ * @method static $this config()
+ * @method static \OSS\OssClient getHttpClient()
+ * @method static string upload()
+ * @method static string getFileName()
+ * @method static string download()
+ * @method static string createBucket(string $bucket, int $ossType = 0, int $ossStorage = 0)
+ * @method static bool isExistBucket(string $bucket)
+ * @method static |null deleteBucket(string $bucket)
+ * @method static bool doesObjectExist(string $object)
+ * @method static |null setFileAcl(string $object, int $acl = 0)
+ * @method static string getFileAcl(string $object)
+ * @method static array listAllFileOfBucket(array $options = [])
+ * @method static string signUrl(string $object, int $timeout)
+ *
  */
 class AliyunOss extends Facade {
 
